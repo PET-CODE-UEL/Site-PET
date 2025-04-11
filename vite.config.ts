1b2809d200172d546b1906903f67ad4ab7f42a00
rodrigomacedo-sjr <rodrigomacedo.sjr@gmail.com>
2025-04-11 11:21:55 -0300

    feat: add tailwindcss

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindscss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  root: 'src/app',
-  plugins: [react()],
-})
+  root: "src/app",
+  plugins: [react(), tailwindscss()],
+});
