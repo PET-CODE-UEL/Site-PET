66433d6b4dce57611e498e5a4b10853773070620
rodrigomacedo-sjr <rodrigomacedo.sjr@gmail.com>
2025-08-20 09:06:51 -0300

    feat: setup tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,8 @@
 import "./App.css";
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
-import Home from "./pages/Home/Home";
-import Projetos from "./pages/Projetos/Projetos.tsx";
-import QuemSomos from "./pages/QuemSomos/QuemSomos.tsx";
-import Acoes from "./pages/Acoes/Acoes.tsx";
-import Eventos from "./pages/Eventos/Eventos.tsx";
+import Navbar from "./components/Navbar";
 
 function App() {
-  return (
-    <BrowserRouter>
-      {/* Navigation */}
-      <nav>
-        <Link to="/">Home</Link> |{" "}
-        <Link to="/quem-somos">Quem Somos</Link> |{" "}
-        <Link to="/projetos">Projetos</Link> |{" "}
-        <Link to="/acoes">Ações</Link> |{" "}
-        <Link to="/eventos">Eventos</Link>
-      </nav>
-
-      {/* Routes */}
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/quem-somos" element={<QuemSomos />} />
-        <Route path="/projetos" element={<Projetos />} />
-        <Route path="/acoes" element={<Acoes />} />
-        <Route path="/eventos" element={<Eventos />} />
-      </Routes>
-    </BrowserRouter>
-  );
+  return <Navbar />;
 }
 
 export default App;
